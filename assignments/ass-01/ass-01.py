dd43ea44eb6808e4a5149ce9de0482d5ec193658
YazCooper <111524103+YazCooper@users.noreply.github.com>
2022-08-23 12:42:18 -0400

    Update ass-01.py

--- assignments/ass-01/ass-01.py
+++ assignments/ass-01/ass-01.py
@@ -4,4 +4,4 @@ from xml.etree.ElementTree import Comment
 print("Yazmin Cooper")
 print("I lived in Hawaii for 3 years")
 print("My favorite thing to do is play video games")
-By Yazmin Cooper
+#By Yazmin Cooper
