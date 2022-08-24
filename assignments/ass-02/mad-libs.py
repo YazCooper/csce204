fd947b00229eb50459770a39d068f51723a4133b
YazCooper <111524103+YazCooper@users.noreply.github.com>
2022-08-24 14:48:14 -0400

    Add files via upload

---
+++ assignments/ass-02/mad-libs.py
@@ -0,0 +1,30 @@
+#Creating MadLibs Game
+#Author Yazmin Cooper
+print("Its time for MadLibs! Please enter the following information when prompted.")
+noun_1 = input("Noun: ")
+noun_2 = input("Noun: ")
+noun_3 = input("Noun: ")
+adjective_1 = input("Adjective: ")
+adjective_2 = input("Adjective: ")
+adjective_3 = input("Adjective: ")
+ing_verb = input("A verb ending in -ing: ")
+p_noun1 = input("Plural Noun: ")
+p_noun2 = input("Plural Noun: ")
+p_noun3 = input("Plural Noun: ")
+a_cloth = input("Article of Clothing: ")
+place = input("A place: ")
+animal = input("Animal: ")
+
+
+print(f"""Where can you meet a nice, normal {noun_1} to date? Sure, there are traditional avenues like the workplace or the {place}, but here are some other creative, {adjective_1} ways:
+- Show off your awesome athletic skills by joining a/an {noun_2}-ball team at the rec center
+- Walk your favorite four-legged {noun_3} at the local {animal} park.
+- Attend a/an {ing_verb} class at a community college.
+- Look for antique {p_noun1} for your collection at a flea market.
+- Suggestively squeeze the largest, juiciest {p_noun2} while exploring the {adjective_2} farmers' market.
+- Do cardio or lift {p_noun3} at the gym in your tighest {a_cloth} - you'll feel fit and look {adjective_3}!
+
+
+""")
+
+
