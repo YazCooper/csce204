bbff3c0254d19b59c2a48dfdd8b9bd8dd58ce605
YazCooper <111524103+YazCooper@users.noreply.github.com>
2022-08-24 14:01:15 -0400

    Delete aug23 directory

--- aug23/more_input.py
+++
@@ -1,7 +0,0 @@
-#Author: Yazmin Cooper
-
-first_name = input("Enter First Name: ")
-last_name = input("Enter Last Name: ")
-print(f"Welcome {first_name} {last_name}")
-
-print("Welcome " + first_name + " " + last_name)
