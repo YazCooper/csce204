af4abefca23143bb273ecdd3f14932baecaffc7a
YazCooper <111524103+YazCooper@users.noreply.github.com>
2022-08-24 13:56:55 -0400

    Add files via upload

---
+++ aug23/more_input.py
@@ -0,0 +1,7 @@
+#Author: Yazmin Cooper
+
+first_name = input("Enter First Name: ")
+last_name = input("Enter Last Name: ")
+print(f"Welcome {first_name} {last_name}")
+
+print("Welcome " + first_name + " " + last_name)
