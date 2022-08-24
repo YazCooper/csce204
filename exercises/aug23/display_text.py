96936424f733f040a62ccd9a1c9393fbcc0d85b1
YazCooper <111524103+YazCooper@users.noreply.github.com>
2022-08-24 14:00:51 -0400

    Add files via upload

---
+++ exercises/aug23/display_text.py
@@ -0,0 +1,13 @@
+print("Welcome to today!")
+
+print("My fav things:")
+print("-Apples")
+print("-Bikes")
+print("-Chalk")
+
+print("""
+    My fav things:
+    -Apples
+    -Bikes
+    -Chalk
+""")
---
+++ exercises/aug23/user_input.py
@@ -0,0 +1,5 @@
+user_name = input("Enter Name: ")
+print("Welcome " + user_name)
+
+
+#print(f"Welcome {user_name}")  (formatted print)
